f0bc3ec7fedb9c81eeb27504076d7b1241960fc0
alissonsouzaac <alissonjscosta@gmail.com>
2023-02-08 17:21:29 -0300

    update

--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -1,1 +1,8 @@
-console.log('oi')
+import { execSync} from 'child_process';
+
+console.log('[DEPLOY PREVIEW] Start!!!')
+const command = "yarn deploy:dev"
+const output = execSync(command, {encoding: "utf8"});
+console.log('[DEPLOY PREVIEW] Env!!!')
+
+console.log(output)
