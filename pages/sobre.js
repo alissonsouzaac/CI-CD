ffbd6ac3025dc8069887f15021d5c3e918883ce3
alissonsouzaac <alissonjscosta@gmail.com>
2023-02-11 13:06:05 -0300

    update

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,7 +1,12 @@
+import NextLink from "next/link";
+
 export default function AboutScreen() {
   return (
     <div>
       <h1>Sobre</h1>
+      <NextLink href="/">
+        <a>Ir para a página incial</a>
+      </NextLink>
     </div>
   );
 }
--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -3,6 +3,9 @@ import { execSync} from 'child_process';
 console.log('[DEPLOY PREVIEW] Start!!!')
 const command = "yarn deploy:dev"
 const output = execSync(command, {encoding: "utf8"});
+const outputLines = output.split("/n")
+const DEPLOY_URL = outputLines[outputLines.length -1]
 console.log('[DEPLOY PREVIEW] Env!!!')
 
+console.log(DEPLOY_URL)
 console.log(output)
